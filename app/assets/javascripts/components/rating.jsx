b65055ef2d000159f813604c19585ecbfa5d74e9
Merlyn <curious.attempt.bunny@gmail.com>
2017-06-15 10:30:50 -0700

    Isolate rating components from each others. Fixes #11.

--- app/assets/javascripts/components/rating.jsx
+++ app/assets/javascripts/components/rating.jsx
@@ -1,17 +1,19 @@
 class Rating extends React.Component {
      render() {
+          var key = Math.random() // TODO FIXME a bit of a hack
+
           return (
                <fieldset className="rate">
-                    <input type="radio" id="rating10" name="rating" value="10" /><label htmlFor="rating10" title="5 stars"></label>
-                    <input type="radio" id="rating9" name="rating" value="9" /><label className="half" htmlFor="rating9" title="4 1/2 stars"></label>
-                    <input type="radio" id="rating8" name="rating" value="8" /><label htmlFor="rating8" title="4 stars"></label>
-                    <input type="radio" id="rating7" name="rating" value="7" /><label className="half" htmlFor="rating7" title="3 1/2 stars"></label>
-                    <input type="radio" id="rating6" name="rating" value="6" /><label htmlFor="rating6" title="3 stars"></label>
-                    <input type="radio" id="rating5" name="rating" value="5" /><label className="half" htmlFor="rating5" title="2 1/2 stars"></label>
-                    <input type="radio" id="rating4" name="rating" value="4" /><label htmlFor="rating4" title="2 stars"></label>
-                    <input type="radio" id="rating3" name="rating" value="3" /><label className="half" htmlFor="rating3" title="1 1/2 stars"></label>
-                    <input type="radio" id="rating2" name="rating" value="2" /><label htmlFor="rating2" title="1 star"></label>
-                    <input type="radio" id="rating1" name="rating" value="1" /><label className="half" htmlFor="rating1" title="1/2 star"></label>
+                    <input type="radio" id={key+'_rating10'} name={key+'_rating"'}value="10" /><label htmlFor={key+'_rating10'} title="5 stars"></label>
+                    <input type="radio" id={key+'_rating9'} name={key+'_rating"'}value="9" /><label className="half" htmlFor={key+'_rating9'} title="4 1/2 stars"></label>
+                    <input type="radio" id={key+'_rating8'} name={key+'_rating"'}value="8" /><label htmlFor={key+'_rating8'} title="4 stars"></label>
+                    <input type="radio" id={key+'_rating7'} name={key+'_rating"'}value="7" /><label className="half" htmlFor={key+'_rating7'} title="3 1/2 stars"></label>
+                    <input type="radio" id={key+'_rating6'} name={key+'_rating"'}value="6" /><label htmlFor={key+'_rating6'} title="3 stars"></label>
+                    <input type="radio" id={key+'_rating5'} name={key+'_rating"'}value="5" /><label className="half" htmlFor={key+'_rating5'} title="2 1/2 stars"></label>
+                    <input type="radio" id={key+'_rating4'} name={key+'_rating"'}value="4" /><label htmlFor={key+'_rating4'} title="2 stars"></label>
+                    <input type="radio" id={key+'_rating3'} name={key+'_rating"'}value="3" /><label className="half" htmlFor={key+'_rating3'} title="1 1/2 stars"></label>
+                    <input type="radio" id={key+'_rating2'} name={key+'_rating"'}value="2" /><label htmlFor={key+'_rating2'} title="1 star"></label>
+                    <input type="radio" id={key+'_rating1'} name={key+'_rating"'}value="1" /><label className="half" htmlFor={key+'_rating1'} title="1/2 star"></label>
                 </fieldset>
           )
      }
